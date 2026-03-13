ac07f14749d8284a00b73889883ad05733631c88
Ziyuli1121 <157041232+Ziyuli1121@users.noreply.github.com>
2026-03-13 18:09:42 +0000

    Deploying to gh-pages from @ Ziyuli1121/Ziyuli1121.github.io@05211222af3840587aea5636d2cbe7ad88bb922e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-blogs",
           title: "Blogs",
-          description: "My personal blogs on Notion",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/blogs/";
